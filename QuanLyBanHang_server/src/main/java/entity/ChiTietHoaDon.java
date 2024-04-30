9f78843cbf1ea4a309f6c56bf1e85915cf07b1ef
Doww <kay.ngoquoctinh@gmail.com>
2024-04-30 15:42:33 +0700

    update chitiet entity

--- QuanLyBanHang_server/src/main/java/entity/ChiTietHoaDon.java
+++ QuanLyBanHang_server/src/main/java/entity/ChiTietHoaDon.java
@@ -2,15 +2,78 @@ package entity;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+@Entity
+@Table(name = "chitiethoadon")
 public class ChiTietHoaDon implements Serializable{
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	@ManyToOne
+	@JoinColumn(name = "maHoaDon" )
 	private HoaDon hoaDon;
+	@ManyToOne
+	@JoinColumn(name = "maHangHoa")
 	private HangHoa hangHoa;
 	private int soLuong;
 	private double donGia;
+	private String donViTinh;
+	public ChiTietHoaDon(HoaDon hoaDon, HangHoa hangHoa, int soLuong, double donGia, String donViTinh) {
+		super();
+		this.hoaDon = hoaDon;
+		this.hangHoa = hangHoa;
+		this.soLuong = soLuong;
+		this.donGia = donGia;
+		this.donViTinh = donViTinh;
+	}
+	public ChiTietHoaDon() {
+		super();
+	}
+	public HoaDon getHoaDon() {
+		return hoaDon;
+	}
+	public void setHoaDon(HoaDon hoaDon) {
+		this.hoaDon = hoaDon;
+	}
+	public HangHoa getHangHoa() {
+		return hangHoa;
+	}
+	public void setHangHoa(HangHoa hangHoa) {
+		this.hangHoa = hangHoa;
+	}
+	public int getSoLuong() {
+		return soLuong;
+	}
+	public void setSoLuong(int soLuong) {
+		this.soLuong = soLuong;
+	}
+	public double getDonGia() {
+		return donGia;
+	}
+	public void setDonGia(double donGia) {
+		this.donGia = donGia;
+	}
+	public String getDonViTinh() {
+		return donViTinh;
+	}
+	public void setDonViTinh(String donViTinh) {
+		this.donViTinh = donViTinh;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	@Override
+	public String toString() {
+		return "ChiTietHoaDon [hoaDon=" + hoaDon + ", hangHoa=" + hangHoa + ", soLuong=" + soLuong + ", donGia="
+				+ donGia + ", donViTinh=" + donViTinh + "]";
+	}
+	
+	
 	
 	
 }
--- QuanLyBanHang_server/src/main/java/entity/PhieuTraHang.java
+++ QuanLyBanHang_server/src/main/java/entity/PhieuTraHang.java
@@ -3,10 +3,80 @@ package entity;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+@Entity
+@Table(name = "phieutrahang")
 public class PhieuTraHang implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private String maPhieuTraHang;
 	private LocalDateTime thoiGianTao;
+	
+	@OneToOne
+	@JoinColumn(name = "maHoaDon")
 	private HoaDon hoaDon;
 	private String ghiChu;
 	private TrangThaiPhieuTraHang trangThaiPhieuTraHang;
+	public PhieuTraHang(String maPhieuTraHang, LocalDateTime thoiGianTao, HoaDon hoaDon, String ghiChu,
+			TrangThaiPhieuTraHang trangThaiPhieuTraHang) {
+		super();
+		this.maPhieuTraHang = maPhieuTraHang;
+		this.thoiGianTao = thoiGianTao;
+		this.hoaDon = hoaDon;
+		this.ghiChu = ghiChu;
+		this.trangThaiPhieuTraHang = trangThaiPhieuTraHang;
+	}
+	public PhieuTraHang() {
+		super();
+	}
+	public String getMaPhieuTraHang() {
+		return maPhieuTraHang;
+	}
+	public void setMaPhieuTraHang(String maPhieuTraHang) {
+		this.maPhieuTraHang = maPhieuTraHang;
+	}
+	public LocalDateTime getThoiGianTao() {
+		return thoiGianTao;
+	}
+	public void setThoiGianTao(LocalDateTime thoiGianTao) {
+		this.thoiGianTao = thoiGianTao;
+	}
+	public HoaDon getHoaDon() {
+		return hoaDon;
+	}
+	public void setHoaDon(HoaDon hoaDon) {
+		this.hoaDon = hoaDon;
+	}
+	public String getGhiChu() {
+		return ghiChu;
+	}
+	public void setGhiChu(String ghiChu) {
+		this.ghiChu = ghiChu;
+	}
+	public TrangThaiPhieuTraHang getTrangThaiPhieuTraHang() {
+		return trangThaiPhieuTraHang;
+	}
+	public void setTrangThaiPhieuTraHang(TrangThaiPhieuTraHang trangThaiPhieuTraHang) {
+		this.trangThaiPhieuTraHang = trangThaiPhieuTraHang;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	@Override
+	public String toString() {
+		return "PhieuTraHang [maPhieuTraHang=" + maPhieuTraHang + ", thoiGianTao=" + thoiGianTao + ", hoaDon=" + hoaDon
+				+ ", ghiChu=" + ghiChu + ", trangThaiPhieuTraHang=" + trangThaiPhieuTraHang + "]";
+	}
+	
+	
 }
